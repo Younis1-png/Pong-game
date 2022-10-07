bd104c490ea814791f875f5a8e071aa9cbd234a4
Younous Saleh <younisssaleh10@gmail.com>
2022-10-07 06:08:32 -0400

    Setup project

---
+++ Pong1.py
@@ -0,0 +1,16 @@
+
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by @ Younis1-png")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+
+
+
+# Main game loop
+while True:
+    # every time our game runs it update the screen 
+    wn.update()    
+
